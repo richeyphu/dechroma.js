b4f90d036aa32feb9f5f020fe6313e593ff138d8
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-06-28 04:46:32 +0700

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,23 @@
+/*!
+ * dechroma.js
+ * Copyright(c) 2023 Phurit D.
+ * MIT Licensed
+ */
+
+type Enumerate<
+  N extends number,
+  Acc extends number[] = []
+> = Acc['length'] extends N
+  ? Acc[number]
+  : Enumerate<N, [...Acc, Acc['length']]>;
+
+type IntRange<F extends number, T extends number> = Exclude<
+  Enumerate<T>,
+  Enumerate<F>
+>;
+
+type RGBValues = IntRange<0, 256>;
+
+type ChromaKeyRange = [RGBValues, RGBValues];
+
+export { ChromaKeyRange };
