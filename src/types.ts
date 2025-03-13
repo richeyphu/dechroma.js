168db3d8884e7d573b27bdb5497db4e6e243e550
akara <55230837+richeyphu@users.noreply.github.com>
2025-03-14 01:32:29 +0700

    style: format with prettier

--- src/types.ts
+++ src/types.ts
@@ -6,7 +6,7 @@
 
 type Enumerate<
   N extends number,
-  Acc extends number[] = []
+  Acc extends number[] = [],
 > = Acc['length'] extends N
   ? Acc[number]
   : Enumerate<N, [...Acc, Acc['length']]>;
